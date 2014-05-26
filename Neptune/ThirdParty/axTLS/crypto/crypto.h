d199e9a6a60b5f0dba1c93ef2a9665991e9353df
huangjq0617 <hjq0617@gmail.com>
2014-05-26 16:34:31 +0800

    fix name conflict

--- Neptune/ThirdParty/axTLS/crypto/crypto.h
+++ Neptune/ThirdParty/axTLS/crypto/crypto.h
@@ -39,13 +39,15 @@
 extern "C" {
 #endif
 
-#define MD5_Init    MD5_Init_NPT
-#define MD5_Update  MD5_Update_NPT
-#define MD5_Final   MD5_Final_NPT
-#define SHA1_Init   SHA1_Init_NPT
-#define SHA1_Update SHA1_Update_NPT
-#define SHA1_Final  SHA1_Final_NPT
-#define RSA_free    RSA_free_NPT
+#define MD5_Init            MD5_Init_NPT
+#define MD5_Update          MD5_Update_NPT
+#define MD5_Final           MD5_Final_NPT
+#define SHA1_Init           SHA1_Init_NPT
+#define SHA1_Update         SHA1_Update_NPT
+#define SHA1_Final          SHA1_Final_NPT
+#define RSA_free            RSA_free_NPT
+#define AES_cbc_encrypt     AES_cbc_encrypt_NPT
+#define AES_cbc_decrypt     AES_cbc_decrypt_NPT
     
 #include "config.h"
 #include "bigint_impl.h"
